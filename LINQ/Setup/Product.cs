5e9c2bdec734ce96df6f440061fa6b80feaeeef9
Anyanwu Raphael <anyanwuraphaelc@gmail.com>
2022-07-04 09:00:46 +0100

    Added Product

---
+++ LINQ/Setup/Product.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ALGOnDS.LINQ.Sequence
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Color { get; set; }
+        public string Brand { get; set; }
+        public int OrderId { get; set; }        
+    }
+
+    public class GetAllProd
+    {
+        public IEnumerable<Product> GetAll()
+        {
+            var products = new List<Product>()
+            {
+                new Product
+                {
+                    Id = 1,
+                    Brand = "HP",
+                    Color = "Pink",
+                    Name = "New HP 1",
+                    OrderId = 1
+                },
+                new Product
+                {
+                    Id = 2,
+                    Brand = "HP",
+                    Color = "Pink",
+                    Name = "New HP 2",
+                    OrderId = 1
+                },
+                new Product
+                {
+                    Id = 3,
+                    Brand = "HP",
+                    Color = "Yellow",
+                    Name = "New HP 2",
+                    OrderId = 2
+                }
+            };
+
+            return products;
+        }
+    }
+}
---
+++ LINQ/Setup/ProductComparer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Text;
+
+namespace ALGOnDS.LINQ.Sequence
+{
+    public class ProductComparer : EqualityComparer<Product>
+    {
+        public override bool Equals([AllowNull] Product x, [AllowNull] Product y)
+        {
+            // Compare each property in both objects
+            return (x.Name == y.Name && x.Color == y.Color);
+        }
+
+        public override int GetHashCode([DisallowNull] Product obj)
+        {
+            string value = obj.Color + obj.Brand + obj.Id.ToString() + obj.Name;
+
+            return value.GetHashCode();
+        }
+    }
+}
