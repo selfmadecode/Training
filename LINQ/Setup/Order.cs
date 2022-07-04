ce5536a3348d0ad07b81b03f6794a0b2bf162d7e
Anyanwu Raphael <anyanwuraphaelc@gmail.com>
2022-07-04 08:59:24 +0100

    Added Order

---
+++ LINQ/Setup/Order.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ALGOnDS.LINQ.Setup
+{
+    public class Order
+    {
+        public int Id { get; set; }
+        public int ProductId { get; set; }
+
+        public IEnumerable<Order> GetAll()
+        {
+            var orders = new List<Order>()
+            {
+                new Order
+                {
+                    Id = 1,
+                    ProductId = 1
+                },
+            };
+
+            return orders;
+        }
+    }
+}
