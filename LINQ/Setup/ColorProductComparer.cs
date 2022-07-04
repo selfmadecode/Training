48f61a5d9ea254c39a389a7fa88deb43c2cadd56
Anyanwu Raphael <anyanwuraphaelc@gmail.com>
2022-07-04 08:58:35 +0100

    Override ProductComparer

---
+++ LINQ/Setup/ColorProductComparer.cs
@@ -0,0 +1,16 @@
+﻿using ALGOnDS.LINQ.Sequence;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Text;
+
+namespace ALGOnDS.LINQ.Setup
+{
+    public class ColorProductComparer : ProductComparer
+    {
+        public override bool Equals([AllowNull] Product x, [AllowNull] Product y)
+        {
+            return (x.Color == y.Color);
+        }
+    }
+}
